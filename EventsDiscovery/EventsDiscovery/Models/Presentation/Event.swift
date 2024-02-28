b8730e505a9778179993b72b1d0791224898d98a
Jader Nunes <jadernunes.jbn@gmail.com>
2024-02-28 12:26:18 -0300

    Created Event Data UI

---
+++ EventsDiscovery/EventsDiscovery/Models/Presentation/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  EventsDiscovery
+//
+//  Created by Jader Nunes on 2024-02-28.
+//
+
+import UIKit
+
+struct Event {
+    let id: String
+    let name: String
+    let classification: String
+    let urlString: String
+}
