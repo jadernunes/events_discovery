2b58cfd0c07519273ec97cee870a403bc6beb8ce
Jader Nunes <jadernunes.jbn@gmail.com>
2024-02-28 20:46:39 -0300

    Created LoaderView component

---
+++ EventsDiscovery/EventsDiscovery/Components/LoaderView.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderView.swift
+//  EventsDiscovery
+//
+//  Created by Jader Nunes on 2024-02-28.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    
+    var body: some View {
+        ProgressView()
+            .controlSize(.large)
+    }
+}
+
