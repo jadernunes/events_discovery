955ee32c7fa262cb7bdf6a7e07f134c737b3cd7c
Jader Nunes <jadernunes.jbn@gmail.com>
2024-02-29 19:48:33 -0300

    Added no internet string

--- EventsDiscovery/EventsDiscovery/Network/NetworkErrorType.swift
+++ EventsDiscovery/EventsDiscovery/Network/NetworkErrorType.swift
@@ -8,5 +8,6 @@
 enum NetworkErrorType: Error {
     case missingURL
     case jsonEncodingFailed(error: Error)
-    case noInternet(message: String = "No internet")
+    case noInternet(message: String = Localize.string(key: "error.noInternet"))
+    case generic(message: String = Localize.string(key: "genericErrorMessagel"))
 }
